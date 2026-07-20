6a7b078c1845be973e0cebbeae24f618abc80731
Jeremy Gutarra <jeremybenjamin.gp@gmail.com>
2026-07-20 17:19:22 -0500

    token deshabilitado por el momento

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -12,10 +12,11 @@ const axiosInstance = axios.create({
 // Agregar token a las peticiones
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
+    // Lo habilitaremos cuando se implemente la autenticación con tokens JWT
+    //const token = localStorage.getItem("token");
+    //if (token) {
+    //  config.headers.Authorization = `Bearer ${token}`;
+    //}
     return config;
   },
   (error) => {
@@ -28,7 +29,7 @@ axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
-      localStorage.removeItem("token");
+      localStorage.removeItem("authUser");
       window.location.href = "/";
     }
     return Promise.reject(error);
