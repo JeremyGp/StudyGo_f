103f9461af9fdebdae79c00bcfce8d70ff892445
Jeremy Gutarra <jeremybenjamin.gp@gmail.com>
2026-07-20 17:28:35 -0500

    Capa de servicios completa, por ahora

---
+++ src/services/horario.js
@@ -0,0 +1,41 @@
+import axiosInstance from "../config/axiosInstance";
+
+export const horarioService = {
+  async crear(idAsignatura, datos) {
+    const response = await axiosInstance.post(
+      `/horarios?id_asignatura=${idAsignatura}`,
+      datos
+    );
+
+    return response.data;
+  },
+
+  async obtenerPorId(idHorario) {
+    const response = await axiosInstance.get(
+      `/horarios/${idHorario}`
+    );
+
+    return response.data;
+  },
+
+  async listar(idAsignatura) {
+    const response = await axiosInstance.get(
+      `/horarios?id_asignatura=${idAsignatura}`
+    );
+
+    return response.data;
+  },
+
+  async actualizar(idHorario, datos) {
+    const response = await axiosInstance.put(
+      `/horarios/${idHorario}`,
+      datos
+    );
+
+    return response.data;
+  },
+
+  async eliminar(idHorario) {
+    await axiosInstance.delete(`/horarios/${idHorario}`);
+  },
+};
